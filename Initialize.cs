3810b277526b238670407bc4409f152b4dae9a6a
Confusified <rogier.hendriksen2005@gmail.com>
2024-03-19 17:24:01 +0100

    Add project files.

---
+++ Initialize.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace FPSSlider
+{
+    [BepInPlugin(modGUID,modName,modVersion)]
+    public class Initialize : BaseUnityPlugin
+    {
+        private const string modGUID = "mod.Confusified.FPSSlider";
+        private const string modName = "FPS Slider";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony _Harmony = new Harmony(modGUID);
+        public static ManualLogSource modLogger;
+        public void Awake()
+        {
+            modLogger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            modLogger = Logger;
+
+            _Harmony.PatchAll();
+        }
+    }
+}
