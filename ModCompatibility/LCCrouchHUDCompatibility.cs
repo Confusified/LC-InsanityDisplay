5f763c3bd10fc1528af5927c6ccab9f5c76670b1
Confusified <60018584+Confusified@users.noreply.github.com>
2024-03-31 20:52:16 +0200

    fix positioning

--- ModCompatibility/LCCrouchHUDCompatibility.cs
+++ ModCompatibility/LCCrouchHUDCompatibility.cs
@@ -4,6 +4,7 @@ namespace InsanityDisplay.ModCompatibility
 {
     public class LCCrouchHUDCompatibility
     {
+        private static Vector3 positionToLocal = new Vector3(-16.2386f, -20.2468f, -12.0928f);
         private static Vector3 localPositionOffset = new Vector3(0, 4, 0);
 
         public static void MoveCrouchHUD()
@@ -11,8 +12,8 @@ namespace InsanityDisplay.ModCompatibility
             GameObject CrouchHUDIcon = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD/TopLeftCorner/Self/CrouchIcon").gameObject;
 
             if (CrouchHUDIcon == null) { Initialise.modLogger.LogError("LCCrouchHud's icon wasn't found"); return; }
-
-            CrouchHUDIcon.transform.localPosition += localPositionOffset;
+            //translate position offset to localPosition (-16.2386 -20.2468 -12.0928)
+            CrouchHUDIcon.transform.localPosition = positionToLocal + localPositionOffset;
         }
     }
 }
