e1ecb42fb365683ad1c99af2ca0076350fddec38
Confusified <60018584+Confusified@users.noreply.github.com>
2024-04-18 16:38:18 +0200

    LCVR compatible

--- ModCompatibility/LethalCompanyVRCompatibility.cs
+++ ModCompatibility/LethalCompanyVRCompatibility.cs
@@ -2,42 +2,39 @@
 using UnityEngine;
 using InsanityDisplay.UI;
 using System.Collections;
-using LCVR.UI;
 using LCVR.Player;
 
 namespace InsanityDisplay.ModCompatibility
 {
-    public class LethalCompanyVRCompatibility //CURRENT STATE: ARMHUD WORKS, NON-ARMHUD WORKS BUT NOT POSITIONED, SIZED AND ROTATED PROPERLY
+    public class LethalCompanyVRCompatibility
     {
-        private static Canvas LeftHandCanvas;
-        private static VRHUD vrHUD;
+
         public static IEnumerator EnableVRCompatibility()
         {
             if (!VRSession.InVR) { yield break; } //Player isn't in VR so no need for the compatibility
             Transform SprintMeterTransform = null;
-            UIHandler.InsanityMeter.transform.SetParent(UIHandler.vanillaSprintMeter.transform, false); //This way i won't have to manually set it's parent properly
+            var meterTransform = UIHandler.InsanityMeter.transform;
+            meterTransform.SetParent(UIHandler.vanillaSprintMeter.transform, false); //This way i won't have to manually set it's parent properly
             yield return new WaitUntil(() => VRSession.Instance != null); //wait until vr instance exists
-            vrHUD = VRSession.Instance.HUD;
 
-            if (!Plugin.Config.DisableArmHUD.Value) //Attach to left hand if ArmHUD is not disabled
+            SprintMeterTransform = meterTransform.parent;
+            meterTransform.SetParent(SprintMeterTransform.parent, false); //Get the parent of the stamina meter, to set the position of the insanity meter properly
+
+            meterTransform.localScale = SprintMeterTransform.localScale * 0.86f; //roughly same size as normal
+            meterTransform.rotation = SprintMeterTransform.rotation;
+
+            meterTransform.parent.Find("SelfRed").gameObject.transform.localPosition =
+            meterTransform.parent.Find("Self").gameObject.transform.localPosition += new Vector3(0, UIHandler.selfLocalPositionOffset.y / 2, UIHandler.selfLocalPositionOffset.x / 2);
+
+            meterTransform.localPosition = SprintMeterTransform.localPosition + new Vector3(0, UIHandler.localPositionOffset.y, UIHandler.localPositionOffset.x); //why is it like this? i don't know. does it work? yes
+            if (!Plugin.Config.DisableArmHUD.Value) //fix position of meter & self when using arm hud
             {
-                LeftHandCanvas = vrHUD.LeftHandCanvas.GetComponent<Canvas>();
-                UIHandler.InsanityMeter.transform.SetParent(LeftHandCanvas.transform, false);
-                SprintMeterTransform = LeftHandCanvas.transform.Find("SprintMeter").gameObject.transform;
+                meterTransform.localPosition -= new Vector3(0, UIHandler.localPositionOffset.y / 2, UIHandler.localPositionOffset.x);
+                meterTransform.parent.Find("SelfRed").gameObject.transform.localPosition =
+                meterTransform.parent.Find("Self").gameObject.transform.localPosition -= new Vector3(0, UIHandler.selfLocalPositionOffset.y / 4, UIHandler.selfLocalPositionOffset.x / 4);
             }
-            else
-            {
-                SprintMeterTransform = UIHandler.InsanityMeter.transform.parent;
-                UIHandler.InsanityMeter.transform.SetParent(SprintMeterTransform.parent, false); //Get the parent of the stamina meter
-            }
-            UIHandler.InsanityMeter.transform.localPosition = SprintMeterTransform.localPosition + (UIHandler.localPositionOffset * 0.18f); //+ new Vector3(.55f, .6f, 0);
+            meterTransform.SetParent(SprintMeterTransform, true); //shouldn't have any negative effects(?) and will hide when LCVR's hud hides
 
-            if (Plugin.Config.DisableArmHUD.Value) { UIHandler.InsanityMeter.transform.localPosition += new Vector3(Plugin.Config.HUDOffsetX.Value, Plugin.Config.HUDOffsetY.Value, 0); } //add offset if not using armHUD
-            UIHandler.InsanityMeter.transform.localScale = SprintMeterTransform.localScale * 0.86f; //roughly same size as normal
-            UIHandler.InsanityMeter.transform.rotation = SprintMeterTransform.rotation;
-
-            Initialise.modLogger.LogInfo($"LOCALPOS:\n{SprintMeterTransform.localPosition} {UIHandler.InsanityMeter.transform.localPosition}");
-            Initialise.modLogger.LogInfo($"ROTATION:\n{SprintMeterTransform.rotation} {UIHandler.InsanityMeter.transform.rotation}");
         }
     }
 }
